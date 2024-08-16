8087e872787a642f31e776636dc8ae8b7a035846
Youssef El Idrissi <professionalidrissi@gmail.com>
2024-08-16 01:55:51 +0100

    fixed smth

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""this doc for module."""
+""" Defines a function that queries the Reddit API and returns
+    the number of subscribers (total) for a given subreddit. Function
+    returns 0 if a n invalid subreddit is given
+"""
+import requests
+
+
 import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
-    """Get number of subs."""
+    """method doc"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
